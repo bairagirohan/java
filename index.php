5f079c53684089418d5885b6255f64e398f7f826
bairagirohan <123975074+bairagirohan@users.noreply.github.com>
2023-02-01 20:02:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 <h1>My first PHP page for webhooks 1234 </h1>
  <h2>this is updated code</h2>
 <?php
-echo "Hello World! FROM OCT BATCH WEBHOOOKKKKKKK testing webhook learning for KUCL batch";
- echo "hello from UNNATI 555555.........";
+echo "Hello World! FROM unnati WEBHOOOKKKKKKK testing webhook learning for KUCL batch";
+ echo "hello from UNNATI ";
 ?>
 
 </body>
